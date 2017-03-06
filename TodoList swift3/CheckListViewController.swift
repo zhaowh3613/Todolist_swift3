39bfd80a80b4ffabb60ec4c8de118a0637907aa3
zhaowh3613 <zhaowh3613@outlook.com>
2017-03-06 22:21:18 +0800

    add data source

--- TodoList swift3/CheckListViewController.swift
+++ TodoList swift3/CheckListViewController.swift
@@ -27,6 +27,15 @@ class CheckListViewController: UITableViewController {
         present(alert, animated: true, completion: nil)
         
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
 
 }
 
